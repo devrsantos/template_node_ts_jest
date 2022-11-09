1d95b0a31893a8bba5122b2ced5b3f38b93ce6cd
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-11-09 14:01:27 -0300

    Estrutura Completa

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,4 +1,4 @@
-import  express from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import { routers } from "../routes/routes";
 
@@ -7,12 +7,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((request, response, next) => {
-    response.append('Access-Control-Allow-Origin', ['*']);
-    response.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    response.append('Access-Control-Allow-Headers', 'Content-Type');
+    response.append("Access-Control-Allow-Origin", ["*"])
+    response.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    response.append("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
-app.use(routers)
+app.use(routers);
 
-export {app}
+export { app };
---
+++ src/tests/testUser.spec.ts
@@ -0,0 +1,8 @@
+import { User } from "@config/User";
+
+test('If exist', () => {
+    const user = new User();
+    user.name = "Renan";
+
+    expect(user.name).toBe("Renan");
+});
