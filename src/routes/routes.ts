3843e35fdf6804d4e1722af20fde150857d61e61
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-11-09 11:29:42 -0300

    Adicionando config TS

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,7 +3,9 @@ import { Router } from "express";
 const routers = Router();
 
 routers.get("/", (request, response) => {
-
+    console.log("Olá");
+    
+    response.json({message: "Hello World"}).sendStatus(200);
 });
 
 export {routers}
